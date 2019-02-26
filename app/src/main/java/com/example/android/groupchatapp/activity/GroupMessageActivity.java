54f06db8d17dc6a1efc0cc83c44986589154691f
yuktachauhan <yuktach20167@gmail.com>
2019-02-26 21:40:01 +0530

    messageAdapter

--- app/src/main/java/com/example/android/groupchatapp/activity/GroupMessageActivity.java
+++ app/src/main/java/com/example/android/groupchatapp/activity/GroupMessageActivity.java
@@ -7,23 +7,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
-
+import com.example.android.groupchatapp.MessageAdatpter;
 import com.example.android.groupchatapp.R;
 import com.example.android.groupchatapp.rest.ApiClient;
 import com.example.android.groupchatapp.rest.ApiInterface;
 
+import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class GroupMessageActivity extends AppCompatActivity {
+public class GroupMessageActivity extends AppCompatActivity implements View.OnClickListener{
 
     private EditText textMsg;
     private Button sendButton;
     private ListView msgListView;
+    private MessageAdatpter messageAdatpter;
+    private final List<String> msgList =new ArrayList<>();
+    private Socket mSocket;
+    {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +41,14 @@ public class GroupMessageActivity extends AppCompatActivity {
         textMsg = (EditText) findViewById(R.id.message);
         sendButton=(Button) findViewById(R.id.msgSendButton);
         msgListView=(ListView) findViewById(R.id.messageListView);
+        messageAdatpter=new MessageAdatpter(msgList,this);
+        msgListView.setAdapter(messageAdatpter);
+        sendButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String message=textMsg.getText().toString().trim();
+
     }
 }
